45393782e36603d152ee9fbc4b3131596a25c4cf
Vivekpkd <62052844+Vivekpkd@users.noreply.github.com>
2020-04-10 13:36:09 +0530

    Add files via upload

---
+++ Nrf24YTest.ino
@@ -0,0 +1,20 @@
+
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+RF24 radio(9,10);
+byte addresses[][6] = {"1Node","2Node"};
+void setup() {
+  Serial.begin(9600);
+  printf_begin();
+  radio.begin();
+radio.openWritingPipe(addresses[0]);
+}
+
+void loop() {
+  Serial.println("This is a test");
+  radio.printDetails();
+  delay(1000);
+}
